3cbf7581b9fa7702d5c7fbacf49ea04cfac16ee4
asela38@gmail.com <asela38@gmail.com>
2015-08-12 14:25:07 +0530

    dummy backend restul server

--- app/backend/server.js
+++ app/backend/server.js
@@ -4,8 +4,37 @@ var app = express();
 app.get('/', function (req, res) {
    res.send('Server is Running');
 
-})
+});
 
+app.get('/address/:id', function (req, res) {
+   var address = {
+   	 id : req.params.id,
+   	 name : 'Asela',
+   	 email : 'asela38@gmail.com',
+   	 telephone : '111222111'
+   }
+   res.type('application/json');
+   res.send(address);
+});
+
+app.get('/address', function (req, res) {
+   var addresses = [
+	   	{
+	   	 id : 1,
+	   	 name : 'Asela',
+	   	 email : 'asela38@gmail.com',
+	   	 telephone : '111222111'
+	   },
+	   {
+	   	 id : 2,
+	   	 name : 'Asela',
+	   	 email : 'asela38@gmail.com',
+	   	 telephone : '111222111'
+	   }
+	];
+   res.type('application/json');
+   res.send(addresses);
+});
 
 
 var server = app.listen(8081, function () {
